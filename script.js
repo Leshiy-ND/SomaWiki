1bd8a48d34626930e418abcdcc2df8aaa1eca7c3
Leshiy-ND <leshiy.industries@gmail.com>
2024-02-12 11:59:50 +0300

    Choise & Consequence handling

--- script.js
+++ script.js
@@ -101,9 +101,48 @@ function updateLocalStorage() {
     else           buttonClean.className = "button-text-disabled";
     
     buttonClean.textContent = textClean;
-    buttonClean.style.float = "right"
+    buttonClean.style.float = "right";
     buttonClean.onclick     = function() { cleanLocalStorage() };
     lsPanel.appendChild(buttonClean);
+}
+
+
+
+function processStoryChoises() {
+    document.querySelectorAll("[story-consequence]").forEach(element => {
+        element.style.color = "#ffddaa";
+    });
+
+    document.querySelectorAll("[story-result]").forEach(elemResult => {
+        const result = elemResult.getAttribute("story-result");
+
+        elemResult.style.display = ("" == result) ? "initial" : "none";
+    });
+
+    document.querySelectorAll("[story-option]").forEach(button => {
+        button.className = (button === button.parentElement.firstElementChild) ? "option-selected" : "option-normal";
+
+        button.addEventListener("click", () => {
+            if (button.className != "option-normal") return;
+
+            const choise = button.parentElement.getAttribute("story-choise");
+            const option = button.getAttribute("story-option");
+
+            for (var otherButton of button.parentElement.children) {
+                otherButton.className = (button === otherButton) ? "option-selected" : "option-normal";
+            }
+
+            document.querySelectorAll("[story-consequence]").forEach(elemConsequence => {
+                const consequence = elemConsequence.getAttribute("story-consequence");
+                if (choise != consequence) return;
+
+                for (var elemResult of elemConsequence.children) {
+                    result = elemResult.getAttribute("story-result");
+                    elemResult.style.display = (option == result) ? "initial" : "none";
+                }
+            });
+        });
+    });
 }
 
 
@@ -301,4 +340,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 Go buy and play <a href="https://store.steampowered.com/app/282140/SOMA/">Soma</a>.`;
             break;
     }
-}); 
+
+
+
+    processStoryChoises();
+});
